5837ea700e3d123f4b0b05b44c0048dda84d7d86
Joachim E. Nebel <joachim.e.nebel@gmail.com>
2024-12-21 15:42:20 +0100

    Done

---
+++ README.md
@@ -0,0 +1,68 @@
+<h> Parsing the file is not included in the runtime. </h>
+
+For the fastest time, run with release profile:
+
+    cargo run -r
+
+To get more stats and pretty print the solutions, run in debug mode:
+
+    cargo run
+
+Result on my Ryzen 5 5600x in release mode:
+
+    7, 9, 1, 3, 8, 4, 6, 2, 5
+    7, 9, 1, 6, 8, 4, 3, 2, 5
+    9, 2, 3, 1, 6, 7, 8, 5, 4
+    9, 2, 6, 1, 3, 7, 8, 5, 4
+
+    48.19µs
+
+When pretty printing in debug mode:
+
+    7, 9, 1, 3, 8, 4, 6, 2, 5
+      y    y    p  
+    B 1 pP 9 gG 5 y
+      G    B    B  
+      g    b    b  
+    Y 3 pP 7 yY 2 p
+      B    G    G  
+      b    g    g  
+    Y 8 pP 4 yY 6 p
+      B    G    B  
+    
+    7, 9, 1, 6, 8, 4, 3, 2, 5
+      y    y    p  
+    B 1 pP 9 gG 5 y
+      G    B    B  
+      g    b    b  
+    Y 6 pP 7 yY 2 p
+      B    G    G  
+      b    g    g  
+    Y 8 pP 4 yY 3 p
+      B    G    B  
+    
+    9, 2, 3, 1, 6, 7, 8, 5, 4
+      p    p    y  
+    g 3 Bb 2 Gg 4 G
+      Y    Y    P  
+      y    y    p  
+    B 1 pP 9 gG 5 y
+      G    B    B  
+      g    b    b  
+    Y 6 pP 7 yY 8 p
+      B    G    B  
+    
+    9, 2, 6, 1, 3, 7, 8, 5, 4
+      p    p    y  
+    g 6 Bb 2 Gg 4 G
+      Y    Y    P  
+      y    y    p  
+    B 1 pP 9 gG 5 y
+      G    B    B  
+      g    b    b  
+    Y 3 pP 7 yY 8 p
+      B    G    B  
+    
+    14500 rotations
+    872 recursions
+    966.153µs
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::{fmt::Display, mem, time::Instant};
+use std::{fmt::Display, fs::read_to_string, time::Instant};
 
 use arrayvec::ArrayVec;
+use const_for::const_for;
 use Color::*;
 use LowerUpper::*;
 
-// 75µs ish
-// printing: 8µs ish
+/*
+    Tile sides:
+    .  A  .
+    D  .  B
+    .  C  .
+*/
+
+/* 
+    Order of tiles:
+    2  1  8
+    3  0  7
+    4  5  6
+*/
+
+const RULES: [Rule; 8] = [
+    // (self's side, other tile idx, other tile side)
+    Rule::new(Side::C, 0, Side::A),
+    Rule::new(Side::B, 1, Side::D),
+    Rule::new(Side::A, 2, Side::C).with_second(Side::B, 0, Side::D),
+    Rule::new(Side::A, 3, Side::C),
+    Rule::new(Side::D, 4, Side::B).with_second(Side::A, 0, Side::C),
+    Rule::new(Side::D, 5, Side::B),
+    Rule::new(Side::C, 6, Side::A).with_second(Side::D, 0, Side::B),
+    Rule::new(Side::C, 7, Side::A).with_second(Side::D, 1, Side::B),
+];
 
 #[derive(Copy, Clone)]
 enum Side {
@@ -44,16 +68,30 @@ impl Part {
     }
 
     fn color(&self) -> Color {
-        unsafe { mem::transmute(self.inner & 0b011) }
+        unsafe { std::mem::transmute(self.inner & 0b011) }
     }
 
     fn lower_upper(&self) -> LowerUpper {
-        unsafe { mem::transmute(self.inner & 0b100) }
+        unsafe { std::mem::transmute(self.inner & 0b100) }
     }
 
     fn matches(&self, other: Part) -> bool {
         // Same color, but different lower/upper
         self.inner ^ other.inner == 0b100
+    }
+
+    fn from(value: char) -> Self {
+        match value {
+            'P' => Part::new(Purple, Upper),
+            'p' => Part::new(Purple, Lower),
+            'G' => Part::new(Green, Upper),
+            'g' => Part::new(Green, Lower),
+            'Y' => Part::new(Yellow, Upper),
+            'y' => Part::new(Yellow, Lower),
+            'B' => Part::new(Blue, Upper),
+            'b' => Part::new(Blue, Lower),
+            _ => panic!("Invalid char '{}'", value)
+        }
     }
 }
 
@@ -74,20 +112,54 @@ impl Display for Part {
 }
 
 // Todo: Pack into a single u16?
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Tile {
     id: u8,
-    parts: [Part; 4]
+    parts: u16
 }
 
 impl Tile {
+    fn new(id: u8, parts_arr: [Part; 4]) -> Self {
+        let mut parts = 0;
+        const_for!(i in 0..4 => {
+            parts |= (parts_arr[i].inner as u16) << (4 * i);
+        });
+        Tile { id, parts }
+    }
+
+    fn new_from_str(id: u8, parts_str: &str) -> Self {
+        if parts_str.len() != 4 {
+            panic!("Invalid tile string length in '{}'", parts_str);
+        }
+
+        let parts = [
+            Part::from(parts_str.as_bytes()[0] as char),
+            Part::from(parts_str.as_bytes()[1] as char),
+            Part::from(parts_str.as_bytes()[2] as char),
+            Part::from(parts_str.as_bytes()[3] as char)
+        ];
+        Tile::new(id, parts)
+    }
+
     fn get(&self, side: Side) -> Part {
-        self.parts[side as usize]
+        let offset = 4 * side as u16;
+        unsafe { std::mem::transmute(((self.parts >> offset) & 0b1111) as u8) }
     }
 
     fn rotate(&mut self) {
-        self.parts.rotate_left(1);
+        self.parts = self.parts.rotate_right(4);
     }
+}
+
+fn read_tiles_from_file(path: &str) -> [Tile; 9] {
+    read_to_string(path).unwrap().lines()
+        .enumerate()
+        .map(|(id, line)| {
+            Tile::new_from_str(id as u8 + 1, line)
+        })
+        .collect::<ArrayVec<Tile, 9>>()
+        .into_inner()
+        .expect("Failed to parse file")
 }
 
 #[derive(Copy, Clone)]
@@ -97,13 +169,110 @@ struct Rule {
     second: Option<(Side, usize, Side)>
 }
 
+impl Rule {
+    const fn new(own_side: Side, other_idx: usize, other_side: Side) -> Self {
+        Rule {
+            first: (own_side, other_idx, other_side),
+            second: None
+        }
+    }
+
+    const fn with_second(mut self, other_side: Side, other_idx: usize, other_tile_side: Side) -> Self {
+        self.second = Some((other_side, other_idx, other_tile_side));
+        self
+    }
+}
+
+fn main() {
+    let tiles = read_tiles_from_file("tiles.txt");
+
+    let before = Instant::now();
+
+    let mut recursions = 0;
+    let mut rotations = 0;
+    let mut prev_tiles = ArrayVec::<Tile, 9>::new();
+
+    for i in 0..tiles.len() {
+        prev_tiles.push(tiles[i]);
+        let mut remaining_tiles = ArrayVec::<Tile, 9>::from(tiles);
+        remaining_tiles.swap_remove(i);
+
+        solve(&mut prev_tiles, remaining_tiles, &mut recursions, &mut rotations);
+
+        prev_tiles.pop();
+    }
+
+    #[cfg(debug_assertions)]
+    println!("{} rotations\n{} recursions\n{:?}", rotations, recursions, before.elapsed());
+
+    #[cfg(not(debug_assertions))]
+    println!("\n{:?}", before.elapsed());
+}
+
+fn solve(prev_tiles: &mut ArrayVec<Tile, 9>, 
+         remaining_tiles: ArrayVec<Tile, 9>, 
+         recursions: &mut usize, rotations: &mut usize) {
+
+    #[cfg(debug_assertions)]
+    { *recursions += 1; }
+    
+    if remaining_tiles.is_empty() {
+        println!("{}", prev_tiles.iter()
+                                 .map(|t| t.id.to_string())
+                                 .collect::<Vec<String>>()
+                                 .join(", "));
+
+        #[cfg(debug_assertions)]
+        print_tiles(&prev_tiles);
+        
+        return;
+    }
+
+    let tile_no = prev_tiles.len();
+    let rule = RULES[tile_no - 1];
+    for i in 0..remaining_tiles.len() {
+        let mut tile = remaining_tiles[i];
+        for _ in 0..4 {
+            #[cfg(debug_assertions)]
+            { *rotations += 1; }
+
+            tile.rotate();
+            let first_ok = check_rule(rule.first, tile, &prev_tiles);
+            let second_ok = match rule.second {
+                Some(second) => check_rule(second, tile, &prev_tiles),
+                None => true,
+            };
+
+            if first_ok && second_ok {
+                let mut remaining_tiles = remaining_tiles.clone();
+                remaining_tiles.swap_remove(i);
+
+                prev_tiles.push(tile);
+                solve(prev_tiles, remaining_tiles, recursions, rotations);
+                prev_tiles.pop();
+            }
+        }
+    }
+}
+
+fn check_rule((own_side, other_idx, other_side): (Side, usize, Side), tile: Tile, tiles: &ArrayVec<Tile, 9>) -> bool {
+    let own_part = tile.get(own_side);
+    let other_part = tiles[other_idx].get(other_side);
+    own_part.matches(other_part)
+}
+
 #[cfg(debug_assertions)]
-fn print_tiles(tiles: &[Tile], order: [usize; 9]) {
+const ORDER: [usize; 9] = [2, 1, 8, 
+                           3, 0, 7, 
+                           4, 5, 6];
+
+#[cfg(debug_assertions)]
+fn print_tiles(tiles: &[Tile]) {
     let mut lines = vec![String::new(); 9];
     for x in 0..3 {
         for y in 0..3 {
             let i = 3 * y + x;
-            let tile = tiles[order[i]];
+            let tile = tiles[ORDER[i]];
             let line0 = y * 3;
             
             lines[line0].push_str(&format!("  {}  ", tile.get(Side::A)));
@@ -117,154 +286,3 @@ fn print_tiles(tiles: &[Tile], order: [usize; 9]) {
     }
     println!()
 }
-
-fn main() {
-    let before = Instant::now();
-    let mut checks = 0;
-
-    let mut prev_tiles = ArrayVec::<Tile, 9>::new();
-
-    for i in 0..TILES.len() {
-        prev_tiles.push(TILES[i]);
-        let mut remaining_tiles = TILES.to_vec();
-        remaining_tiles.swap_remove(i);
-        rec_solve(&mut prev_tiles, &remaining_tiles, &mut checks);
-        prev_tiles.pop();
-    }
-
-    println!("{checks} rotations in {:?}", before.elapsed())
-}
-
-fn rec_solve(prev_tiles: &mut ArrayVec<Tile, 9>, remaining_tiles: &Vec<Tile>, rotations: &mut usize) {
-    if remaining_tiles.is_empty() {
-        println!("{}", prev_tiles.iter().map(|t| t.id.to_string()).collect::<Vec<String>>().join(", "));
-        #[cfg(debug_assertions)]{
-            print_tiles(&prev_tiles, [2, 1, 8, 
-                                      3, 0, 7, 
-                                      4, 5, 6]);
-        }
-        return;
-    }
-
-    let tile_no = prev_tiles.len();
-    let rule = RULES[tile_no - 1];
-    for i in 0..remaining_tiles.len() {
-        let mut tile = remaining_tiles[i];
-        for _ in 0..4 {
-            *rotations += 1;
-            tile.rotate();
-            let first_ok = check_rule(rule.first, tile, &prev_tiles);
-            let second_ok = match rule.second {
-                Some(second) => check_rule(second, tile, &prev_tiles),
-                None => true,
-            };
-
-            if first_ok && second_ok {
-                let mut remaining_tiles = remaining_tiles.clone();
-                remaining_tiles.swap_remove(i);
-
-                prev_tiles.push(tile);
-                rec_solve(prev_tiles, &mut remaining_tiles, rotations);
-                prev_tiles.pop();
-            }
-        }
-    }
-}
-
-fn check_rule((own_side, other_idx, other_side): (Side, usize, Side), tile: Tile, tiles: &ArrayVec<Tile, 9>) -> bool {
-    let own_part = tile.get(own_side);
-    let other_part = tiles[other_idx].get(other_side);
-    own_part.matches(other_part)
-}
-
-const RULES: [Rule; 8] = [
-    Rule {
-        first: (Side::C, 0, Side::A),
-        second: None
-    },
-    Rule {
-        first: (Side::B, 1, Side::D),
-        second: None
-    },
-    Rule {
-        first: (Side::A, 2, Side::C),
-        second: Some((Side::B, 0, Side::D))
-    },
-    Rule {
-        first: (Side::A, 3, Side::C),
-        second: None
-    },
-    Rule {
-        first: (Side::D, 4, Side::B),
-        second: Some((Side::A, 0, Side::C))
-    },
-    Rule {
-        first: (Side::D, 5, Side::B),
-        second: None
-    },
-    Rule {
-        first: (Side::C, 6, Side::A),
-        second: Some((Side::D, 0, Side::B))
-    },
-    Rule {
-        first: (Side::C, 7, Side::A),
-        second: Some((Side::D, 1, Side::B))
-    },
-];
-
-const TILES: [Tile; 9] = [
-    Tile {parts: [
-        Part::new(Green, Lower), 
-        Part::new(Purple, Lower), 
-        Part::new(Blue, Upper), 
-        Part::new(Yellow, Upper)
-    ], id: 1},
-    Tile {parts: [
-        Part::new(Yellow, Lower), 
-        Part::new(Purple, Lower), 
-        Part::new(Green, Upper), 
-        Part::new(Blue, Upper)
-    ], id: 2},
-    Tile {parts: [
-        Part::new(Blue, Lower), 
-        Part::new(Purple, Lower), 
-        Part::new(Green, Upper), 
-        Part::new(Yellow, Upper)
-    ], id: 3},
-    Tile {parts: [
-        Part::new(Green, Lower), 
-        Part::new(Yellow, Lower), 
-        Part::new(Green, Upper), 
-        Part::new(Purple, Upper)
-    ], id: 4},
-    Tile {parts: [
-        Part::new(Purple, Lower), 
-        Part::new(Yellow, Lower), 
-        Part::new(Blue, Upper), 
-        Part::new(Green, Upper)
-    ], id: 5},
-    Tile {parts: [
-        Part::new(Green, Lower), 
-        Part::new(Purple, Lower), 
-        Part::new(Blue, Upper), 
-        Part::new(Yellow, Upper)
-    ], id: 6},
-    Tile {parts: [
-        Part::new(Blue, Lower), 
-        Part::new(Yellow, Lower), 
-        Part::new(Green, Upper), 
-        Part::new(Purple, Upper)
-    ], id: 7},
-    Tile {parts: [
-        Part::new(Blue, Lower), 
-        Part::new(Purple, Lower), 
-        Part::new(Blue, Upper), 
-        Part::new(Yellow, Upper)
-    ], id: 8},
-    Tile {parts: [
-        Part::new(Yellow, Lower), 
-        Part::new(Green, Lower), 
-        Part::new(Blue, Upper), 
-        Part::new(Purple, Upper)
-    ], id: 9},
-];
